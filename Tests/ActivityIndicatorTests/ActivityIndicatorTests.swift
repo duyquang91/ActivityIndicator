d5c1b5dc78b914c63745321095cb7d82abbae356
Steve <steve_dao@ntucenterprise.sg>
2021-02-22 00:00:50 +0700

    added unit test

--- Tests/ActivityIndicatorTests/ActivityIndicatorTests.swift
+++ Tests/ActivityIndicatorTests/ActivityIndicatorTests.swift
@@ -3,14 +3,58 @@ import Combine
 @testable import ActivityIndicator
 
 final class ActivityIndicatorTests: XCTestCase {
-    func testActivityIndicator() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        let activityIndicator = ActivityIndicator()
-        
+    
+    private var disposeBag = Set<AnyCancellable>()
+    
+    override func setUp() {
+        super.setUp()
+        disposeBag = Set<AnyCancellable>()
     }
-
+    
+    override func tearDown() {
+        super.tearDown()
+        disposeBag = Set<AnyCancellable>()
+    }
+    
+    func testActivityIndicator() {
+        var internalDisposeBag = Set<AnyCancellable>()
+        var resultCount = 0
+        var loadingState = [Bool]()
+        let activityIndicator = ActivityIndicator()
+        let expectation = XCTestExpectation()
+        let requestAction = PassthroughSubject<Void, Never>()
+        
+        activityIndicator.loading
+            .handleEvents(receiveCancel: {
+                XCTAssertEqual(loadingState, [false, true, false])
+                expectation.fulfill()
+            })
+            .sink(receiveValue: { value in
+                print(value ? "Loading ..." : "Finished")
+                loadingState.append(value)
+            })
+            .store(in: &internalDisposeBag)
+        
+        requestAction
+            .flatMap { Just("API calling").delay(for: .seconds(Int.random(in: 1...3)), scheduler: RunLoop.main) .trackActivity(activityIndicator)
+            }.handleEvents(receiveCompletion: { _ in
+                XCTAssertEqual(resultCount, 3)
+                internalDisposeBag = Set<AnyCancellable>()
+            })
+            .sink { string in
+                resultCount += 1
+                print(string)
+                if resultCount == 3 { requestAction.send(completion: .finished) }
+            }
+            .store(in: &disposeBag)
+        
+        requestAction.send(())
+        requestAction.send(())
+        requestAction.send(())
+        
+        wait(for: [expectation], timeout: 5000)
+    }
+    
     static var allTests = [
         ("testExample", testActivityIndicator),
     ]
